36a7c810f70cd383fa95f95b9c67ce89a0a6510c
Pratyush Sudhakar <80096811+pratyush1712@users.noreply.github.com>
2026-05-01 17:46:34 -0400

    Refactor import statements in email_service package

--- app/email_service/__init__.py
+++ app/email_service/__init__.py
@@ -1,10 +1,16 @@
 """Email service package."""
 
-from .send_email import (
+from app.email_service.send_email import (
 	send_auditor_invite_email,
 	send_auditor_credentials_email,
 	send_manager_invite_email,
 	send_verification_email,
+)
+
+from app.email_service.templates import (
+	credentials_html,
+	invite_html,
+	verification_html
 )
 
 __all__ = [
@@ -12,4 +18,7 @@ __all__ = [
 	"send_auditor_invite_email",
 	"send_manager_invite_email",
 	"send_verification_email",
+	"credentials_html",
+	"invite_html",
+	"verification_html"
 ]
