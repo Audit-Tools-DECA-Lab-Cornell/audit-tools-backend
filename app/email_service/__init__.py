d4387885ac305f33a6c17fb06077858372acd60d
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2026-06-05 18:27:03 -0400

    Update email and SMS delivery helpers

--- app/email_service/__init__.py
+++ app/email_service/__init__.py
@@ -4,19 +4,28 @@ from app.email_service.send_email import (
 	send_audit_submit_failure_email,
 	send_auditor_invite_email,
 	send_auditor_credentials_email,
+	send_export_ready_email,
 	send_manager_invite_email,
 	send_verification_email,
 )
 
-from app.email_service.templates import credentials_html, invite_html, submit_failure_html, verification_html
+from app.email_service.templates import (
+	credentials_html,
+	export_ready_html,
+	invite_html,
+	submit_failure_html,
+	verification_html,
+)
 
 __all__ = [
 	"send_audit_submit_failure_email",
 	"send_auditor_credentials_email",
 	"send_auditor_invite_email",
+	"send_export_ready_email",
 	"send_manager_invite_email",
 	"send_verification_email",
 	"credentials_html",
+	"export_ready_html",
 	"invite_html",
 	"submit_failure_html",
 	"verification_html",
